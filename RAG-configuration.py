55c8b62cd4e278a8e54d63c6a4b297bf92ff914b
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2025-05-27 16:28:06 +0700

    Add RAG configuration for document loading and vector storage

---
+++ RAG-configuration.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import SupabaseVectorStore
+from langchain_community.embeddings import OllamaEmbeddings
+from supabase.client import Client, create_client
+
+# load environment variables
+load_dotenv()  
+
+# initiate supabase db
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# initiate embeddings model
+embeddings = OllamaEmbeddings(model="bge-m3:latest")
+
+# load pdf docs from folder 'documents'
+loader = PyPDFDirectoryLoader("documents")
+
+# split the documents in multiple chunks
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+docs = text_splitter.split_documents(documents)
+
+# store chunks in vector store
+vector_store = SupabaseVectorStore.from_documents(
+    docs,
+    embeddings,
+    client=supabase,
+    table_name="documents",
+    query_name="match_documents",
+    chunk_size=1000,
+)
